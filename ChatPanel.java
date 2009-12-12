131aca6735f281a8812cc9ba90444a571e4cc24f
Leandro Camargo <leandroico@gmail.com>
2009-12-12 05:17:23 -0200

    making the method reponsible to send the msg to GUI

--- ChatPanel.java
+++ ChatPanel.java
@@ -8,10 +8,12 @@ import java.util.*;
 
 public class ChatPanel extends javax.swing.JPanel {
 
-  private JPanel bodyPanel, bottomPanel;
+  private JPanel bodyPanel, bottomPanel, msgsPanel;
   private ChatInfo chatInfo;
   private JFrame frame;
+  private JTextField txtMessage;
   private ArrayList<String> userNames;
+  private JScrollPane scroll;
 
   public ChatPanel(ChatInfo info) {
     chatInfo  = info;
@@ -24,7 +26,6 @@ public class ChatPanel extends javax.swing.JPanel {
   }
 
   public void buildMainStructure() {
-    JLabel lbl;
     JTextField txt;
     JButton btn;
     JScrollPane scrollPane;
@@ -39,10 +40,12 @@ public class ChatPanel extends javax.swing.JPanel {
     bodyPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
     bodyPanel.setPreferredSize(new Dimension(800, 460));
     // INI.container de mensagens
-    lbl = new JLabel();
-    lbl.setPreferredSize(new Dimension(600, 460));
-    lbl.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 1, new Color(0x666666)));
-    bodyPanel.add(lbl);
+    msgsPanel = new JPanel();
+    msgsPanel.setLayout(new BoxLayout(msgsPanel, BoxLayout.Y_AXIS));
+    scroll = new JScrollPane(msgsPanel);
+    scroll.setPreferredSize(new Dimension(600, 460));
+    scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+    bodyPanel.add(scroll);
     // FIM.container de mensagens
     // INI.lista de usuarios
     usrList = new JList(userNames.toArray());
@@ -54,18 +57,68 @@ public class ChatPanel extends javax.swing.JPanel {
     add(bodyPanel);
     // FIM.o corpo do chat
 
-    bottomPanel = new JPanel();
-    bottomPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+    bottomPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
     bottomPanel.setPreferredSize(new Dimension(800, 40));
     bottomPanel.setBackground(new Color(0xcccccc));
     bottomPanel.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, new Color(0x666666)));
-    txt = new JTextField();
-    txt.setPreferredSize(new Dimension(660, 27));
+    txtMessage = new JTextField();
+    txtMessage.setPreferredSize(new Dimension(660, 27));
     //bottomPanel.add(Box.createRigidArea(new Dimension(20,0)));
-    bottomPanel.add(txt);
+    bottomPanel.add(txtMessage);
     btn = new JButton("Enviar");
     bottomPanel.add(btn);
+    btn.addMouseListener(new MouseAdapter() {
+      public void mouseReleased(MouseEvent e) {
+        addMessage();
+      }
+    });
+
+    // filtra-se o evento de fechar o programa
+    frame.addWindowListener(new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        System.out.println("Desconctando...");
+        // fazer a chamada para desconectar do servidor
+      }
+    });
     add(bottomPanel);
+  }
+
+  private JPanel buildMessageItem(String user, String msg) {
+    JPanel itemPanel;
+    JLabel lbl;
+
+    itemPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+    itemPanel.setMinimumSize(new Dimension(660, 20));
+    lbl = new JLabel(user);
+    lbl.setPreferredSize(new Dimension(150, 20));
+    lbl.setHorizontalAlignment(JLabel.RIGHT);
+    lbl.setVerticalAlignment(JLabel.TOP);
+    itemPanel.add(lbl);
+    itemPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+    lbl = new JLabel(msg);
+    lbl.setVerticalAlignment(JLabel.TOP);
+    lbl.setPreferredSize(new Dimension(440, 20));
+    itemPanel.add(lbl);
+    itemPanel.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, new Color(0x444444)));
+    return itemPanel;
+  }
+
+  private boolean addMessage() {
+    String message = txtMessage.getText().trim();
+    if(message.length() == 0) {
+      JOptionPane.showMessageDialog(frame, "Voce nao pode enviar uma mensagem vazia.");
+      txtMessage.requestFocus();
+      return false;
+    }
+    msgsPanel.add(buildMessageItem(chatInfo.getUsrNome(), message));
+    scroll.setViewportView(msgsPanel);
+    txtMessage.setText("");
+    txtMessage.requestFocus();
+    return true;
+  }
+
+  private boolean sendMessage() {
+    return true;
   }
 
 }
