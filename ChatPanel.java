486aa083ce10cf038665ee5e708ab3064ce76a6a
Leandro Camargo <leandroico@gmail.com>
2009-12-12 01:04:38 -0200

    building up the whole GUI interface for the chat window

--- ChatPanel.java
+++ ChatPanel.java
@@ -16,9 +16,40 @@ public class ChatPanel extends javax.swing.JPanel {
     frame = info.getFrame();
     frame.setTitle(String.format("Usuario '%s' conectado", info.getUsrNome()));
 
+    buildMainStructure();
+  }
+
+  public void buildMainStructure() {
+    JLabel lbl;
+    JTextField txt;
+    JButton btn;
+
     setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
     setOpaque(true);
     setPreferredSize(new Dimension(800,500));
+
+    bodyPanel = new JPanel();
+    bodyPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+    bodyPanel.setPreferredSize(new Dimension(800, 460));
+    lbl = new JLabel();
+    lbl.setPreferredSize(new Dimension(600, 460));
+    lbl.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 1, new Color(0x666666)));
+    bodyPanel.add(lbl);
+    //bodyPanel.setBackground(new Color(0x113377));
+    add(bodyPanel);
+
+    bottomPanel = new JPanel();
+    bottomPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+    bottomPanel.setPreferredSize(new Dimension(800, 40));
+    bottomPanel.setBackground(new Color(0xcccccc));
+    bottomPanel.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, new Color(0x666666)));
+    txt = new JTextField();
+    txt.setPreferredSize(new Dimension(660, 27));
+    //bottomPanel.add(Box.createRigidArea(new Dimension(20,0)));
+    bottomPanel.add(txt);
+    btn = new JButton("Enviar");
+    bottomPanel.add(btn);
+    add(bottomPanel);
   }
 
 }
