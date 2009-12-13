da4dab37dfc048f75dd356797dcf4295a50384e7
Leandro Camargo <leandroico@gmail.com>
2009-12-13 18:30:45 -0200

    adding comodities files

---
+++ generate-client-jar.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+jar cvfm ChatClientJar.jar manifest-client.txt ChatClientPing.class ChatInfo.class ChatMessage.class ChatPane*.class ChatService.class ChatServiceClient.class ChatUserResponsePacket.class EntranceFor*.class MainWindow.class
---
+++ generate-server-jar.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+jar cvfm ChatServerJar.jar manifest-server.txt ChatMessage.class ChatUserResponsePacket.class ChatService.class ChatServiceServer.class
